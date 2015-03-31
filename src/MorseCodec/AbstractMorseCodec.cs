00e4ba3826242b1e88e76190c46d13a074e7aa08
William Boga IV <williambogaiv@gmail.com>
2015-03-31 19:37:17 -0400

    First try at providing a morse implementation. Current behavior has skeleton for what is needed to encode a message based on a character map.

---
+++ src/MorseCodec/AbstractMorseCodec.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MorseCodec
+{
+    public abstract class AbstractMorseCodec : IMorseCode
+    {
+        public abstract char CharacterSeparator { get; }
+
+        public abstract char DitCharacter { get; }
+
+        public abstract char DahCharacter { get; }
+
+        public abstract IDictionary<char, string> CharacterMap { get; }
+
+        public virtual string Decode(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual string Encode(
+            string message,
+            bool ignoreInvalidCharacters = true)
+        {
+            if (!ignoreInvalidCharacters)
+            {
+                if (message
+                    .Select(x => CharacterMap.ContainsKey(x))
+                    .Where(x => x == false)
+                    .Count() > 0)
+                {
+                    throw new FormatException("message contains invalid characters");
+                }
+            }
+
+            var decodedMessage = "";
+
+            foreach (var character in message)
+            {
+                if (character == CharacterSeparator)
+                {
+                    var iterationCount = decodedMessage.Last() == CharacterSeparator
+                        ? 5
+                        : 6;
+
+                    decodedMessage += new string(CharacterSeparator, iterationCount);
+                }
+                else
+                {
+                    var encodedCharacter = CharacterMap.FirstOrDefault(x => x.Key == character);
+
+                    if (!encodedCharacter.Equals(default(KeyValuePair<char, string>)))
+                    {
+                        decodedMessage += encodedCharacter.Value + CharacterSeparator;
+                    }
+                }
+            }
+
+            return decodedMessage.Trim();
+        }
+    }
+}
---
+++ src/MorseCodec/IMorseCode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace MorseCodec
+{
+    public interface IMorseCode
+    {
+        char CharacterSeparator { get; }
+        char DitCharacter { get; }
+        char DahCharacter { get; }
+        IDictionary<char, string> CharacterMap { get; }
+
+        string Decode(string message);
+        string Encode(string message, bool ignoreInvalidCharacters);
+    }
+}
